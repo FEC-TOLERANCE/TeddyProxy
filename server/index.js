375cfe44378f54ca01dd4dd6df29c289577442b3
Teddy Kim <teddykim09@gmail.com>
2020-10-11 10:55:12 -0700

    Added axios dependency and set up routing path for community component

--- server/index.js
+++ server/index.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const app = express();
 const port = 3010;
+const axios = require('axios');
 
-app.use(express.static(__dirname + '/../client'));
+app.use('/:projectId', express.static(__dirname + '/../client'));
+
+app.get('/community/:projectId', (req, res) => {
+  let projectId = req.params.projectId;
+
+  axios.get(`http://localhost:3000/community/${projectId}`)
+    .then(locations => {
+      res.status(200).send(locations.data);
+    })
+    .catch(err => {
+      res.status(500).send(err);
+    })
+})
 
 app.listen(port, () => {
   console.log(`app listening at http://localhost:${port}`)

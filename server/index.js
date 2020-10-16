304a0ff74cffc4cf014589b335950213541a3863
Teddy Kim <teddykim09@gmail.com>
2020-10-15 20:18:46 -0700

    Added all component bundles to render from proxy server

--- server/index.js
+++ server/index.js
@@ -5,18 +5,6 @@ const axios = require('axios');
 
 app.use('/:projectId', express.static(__dirname + '/../client'));
 
-app.get('/community/:projectId', (req, res) => {
-  let projectId = req.params.projectId;
-
-  axios.get(`http://localhost:3000/community/${projectId}`)
-    .then(locations => {
-      res.status(200).send(locations.data);
-    })
-    .catch(err => {
-      res.status(500).send(err);
-    })
-})
-
 app.listen(port, () => {
   console.log(`app listening at http://localhost:${port}`)
 });
